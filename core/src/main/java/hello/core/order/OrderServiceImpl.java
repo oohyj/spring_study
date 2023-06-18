810f21b0dfde29657259725d6a6025d01f1fe737
oohyj <hyj0660@gmail.com>
2023-06-18 20:33:35 +0900

    initial

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,32 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    //구체적인 클래스에 대해서 OrderServiceImpl은 전혀 모름
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy  discountPolicy;
+    //private final DiscountPolicy  discountPolicy = new RateDiscountPolicy();
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    //주문 생성 요청이 오면
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        //회원 정보를 조회를 하고
+        Member member = memberRepository.findById(memberId);
+        //할인에 대해서는 잘 모르겠어, discountPolicy가 알아서 해줘 , 결과만 알려줘
+        //할인 정책에다가 회원을 넘기는 거임 뭘 넘길지는 내가 고민을 하면 됨
+        int discountPrice =discountPolicy.discount(member,itemPrice);
+        return new Order(memberId , itemName , itemPrice, discountPrice);
+    }
+}
---
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.member;
+
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService;
+
+    @BeforeEach //테스트가 실행하기 전에 무조건 실행이 되는
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "memberA",Grade.VIP);
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
