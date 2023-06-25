6934ee25b4a2113dccdae1ff79447ab6c3ed56dc
oohyj <hyj0660@gmail.com>
2023-06-25 19:01:39 +0900

    week 4

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //애플리케이션 전체를 설정하고 구성함
+@Configuration //설정 정보
 public class AppConfig {
 
+    @Bean //스프링 컨테이너에 등록이 됨
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         //   return new FixDiscountPolicy();
         return new RateDiscountPolicy();
